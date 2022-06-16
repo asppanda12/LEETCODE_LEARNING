8843b4433160ef43b3ac96965eb19d05fdb5ca6d
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-06-16 12:28:58 +0530

    Time: 0 ms (100.00%), Space: 61.2 MB (5.65%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,48 +1,19 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) 
-    {
-        int n=nums.length;
-        int lo=0;int hi=n-1;
-        while(lo<hi)
-        {
-            int mid=(lo+hi)/2;
-            if(mid%2==0)
-            {
-                if(mid>0 && nums[mid]==nums[mid-1])
-                {
-                    hi=mid-1;
-                }
-                else
-                if(mid<n && nums[mid]==nums[mid+1])
-                {
-                    lo=mid+1;
-                }
-                else
-                {
-                    return nums[mid];
-                }
-
-            }
-            else
-            {
-                
-                if(mid>0 && nums[mid]==nums[mid-1])
-                {
-                    lo=mid+1;
-                }
-                else
-                if(mid<n && nums[mid]==nums[mid+1])
-                {
-                    hi=mid-1;
-                }
-                else
-                {
-                    return nums[mid];
-                }
-
-            }
-        }
-        return nums[lo];
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1) return nums[0];
+        int l = 0;
+        int h = nums.length-1;
         
+        while(l<h){
+            int mid = l+(h-l)/2;      // divide the array
+            
+            if(nums[mid]==nums[mid+1]) mid = mid-1;      //two same elements should be in same half
+            
+            if((mid-l+1)%2!=0) h = mid;            // checking the length of left half. If its is odd then update ur right pointer to mid
+            
+            else l = mid+1;    // else your right half will be odd then update your left pointer to mid+1
+        }
+        
+        return nums[l];     //left pointer will have the answer at last
     }
 }
