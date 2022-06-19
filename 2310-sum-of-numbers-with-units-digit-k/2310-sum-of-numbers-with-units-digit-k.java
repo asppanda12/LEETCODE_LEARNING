7b124b1cdb1ea51e23da957d895498f3084b5fe2
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-06-19 17:47:05 +0530

    Time: 94 ms (16.67%), Space: 64.2 MB (16.67%) - LeetHub

---
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.java
@@ -0,0 +1,52 @@
+class Solution
+{
+    public static int val(ArrayList<Integer> arr,int sum,int dp[][],int n)
+    {
+        if(sum==0)
+        {
+            return 0;
+        }
+        if(n==0 || sum<0)
+        {
+            return Integer.MAX_VALUE;
+        }
+        if(dp[n][sum]!=-1)
+        {
+            return dp[n][sum];
+        }
+        if(val(arr,sum-arr.get(n-1),dp,n)!=Integer.MAX_VALUE)
+        return dp[n][sum]=Math.min(1+val(arr,sum-arr.get(n-1),dp,n),val(arr,sum,dp,n-1));
+        else
+        return dp[n][sum]=Math.min(val(arr,sum-arr.get(n-1),dp,n),val(arr,sum,dp,n-1));
+    }
+    public int minimumNumbers(int num, int k) 
+    {
+        
+        ArrayList<Integer> arr=new ArrayList<Integer>();
+        for(int i=1;i<=num;i++)
+        {
+            if(i%10==k)
+            {
+                arr.add(i);
+            }
+        }
+        if(num==0)
+        {
+            return 0;
+        }
+        if(arr.size()==0)
+        {
+            return -1;
+        }
+        
+        System.out.println(arr);
+        int dp[][]=new int[arr.size()+2][num+10];
+        for(int i=0;i<=arr.size();i++)
+        {
+        Arrays.fill(dp[i],-1);
+        }
+        int kp=val(arr,num,dp,arr.size());
+        return val(arr,num,dp,arr.size())==Integer.MAX_VALUE?-1:kp;
+        
+    }
+}
