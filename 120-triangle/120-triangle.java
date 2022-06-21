00d86948d903e971bca4959cba843d3b80d7f422
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-06-21 16:08:01 +0530

    Time: 1 ms (99.84%), Space: 43.7 MB (67.10%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,42 +1,27 @@
 class Solution 
 {
-    public static int val(List<List<Integer>> triangle,int i,int j,int n,int dp[][])
+    public int val(List<List<Integer>> triangle,int i,int j,int n,int dp[][])
     {
-        if(i==n-1 && j<triangle.get(i).size())
+        if(i==n)
         {
-            return triangle.get(i).get(j);
+            return 0;
         }
-        
-        
         if(dp[i][j]!=-1)
         {
             return dp[i][j];
         }
-        int min=Integer.MAX_VALUE;
-        if(i+1<n)
-        {
-                
-            min=Math.min(val(triangle,i+1,j,n,dp),min);
-        }
-        if(i+1<n && j+1<triangle.get(i+1).size())
-        {
-              min=Math.min(val(triangle,i+1,j+1,n,dp),min);
-        }
-    
-        dp[i][j]=min==Integer.MAX_VALUE?min:triangle.get(i).get(j)+min;
-       
-        return dp[i][j];
+        int  a=triangle.get(i).get(j)+val(triangle,i+1,j,n,dp);
+        int b=triangle.get(i).get(j)+val(triangle,i+1,j+1,n,dp);
+        return dp[i][j]=Math.min(a,b);
     }
     public int minimumTotal(List<List<Integer>> triangle) 
     {
-        
-        
-        int dp[][]=new int[206][206];
-        for(int i=0;i<206;i++)
+        int n=triangle.size();
+        int dp[][]=new int[n+1][triangle.get(n-1).size()+1];
+        for(int i=0;i<=n;i++)
         {
             Arrays.fill(dp[i],-1);
         }
-        return val(triangle,0,0,triangle.size(),dp);
-        
+         return val(triangle,0,0,triangle.size(),dp);
     }
 }
