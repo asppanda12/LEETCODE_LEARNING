8be5d61cc669b3a1d24fd13dd329f84d442e58a8
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-06-29 12:54:24 +0530

    Time: 7 ms (82.32%), Space: 42.6 MB (87.14%) - LeetHub

---
+++ 909-snakes-and-ladders/909-snakes-and-ladders.java
@@ -0,0 +1,68 @@
+class Solution 
+{
+    class Pair
+    {
+        int x;int y;
+        Pair(int x,int y)
+        {
+            this.x=x;
+            this.y=y;
+        }
+    }
+    public Pair conv(int val,int n)
+    {
+       int i=(val%n==0)?val/n-1:val/n;
+       int x=n-1-i;
+       int j=val%n==0?n-1:val%n-1;
+       if((i+1)%2==0)
+       j=n-1-j;
+       Pair a=new Pair(x,j);
+        return a;
+    }
+    public int snakesAndLadders(int[][] board) 
+    {
+        Queue<Pair> qu=new LinkedList<Pair>();
+        int n=board.length;
+        int v[]=new int[n*n+10];
+        qu.add(new Pair(1,0));
+        v[1]=1;
+                HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+
+        
+        while(qu.isEmpty()==false)
+        {
+            int t=qu.peek().x;
+                 int dist=qu.peek().y;
+            qu.poll();
+            if(t==n*n)
+            {
+               return dist;
+            }
+             for(int i=t+1;i<=Math.min(n*n,t+6);i++)
+            {
+                if(v[i]==0)
+                {
+                    v[i]=1;
+                    Pair tp=conv(i,n);
+                    int x=tp.x;
+                    int y=tp.y;
+                    if(board[x][y]==-1)
+                    {
+                        qu.add(new Pair(i,dist+1));
+                         map.put(i,t);
+                    }
+                    else
+                    {
+                         qu.add(new Pair(board[x][y],dist+1));
+                         map.put(board[x][y],t);
+                    }
+                }
+            }
+           
+        
+        }
+            
+        return -1;
+        
+    }
+}
