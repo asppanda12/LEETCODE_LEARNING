3557ed43508681b9d086f673d28c70aef1edf620
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-07-05 00:43:25 +0530

    Time: 4 ms (68.90%), Space: 42.8 MB (28.84%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,6 +1,6 @@
 class Solution
 {
-    public int isLetter(String s1,String s2,String s3,int i,int j,int k,int dp[][][])
+    public int isLetter(String s1,String s2,String s3,int i,int j,int k,int dp[][])
     {
         
         if(i==s1.length() && j==s2.length() && k==s3.length())
@@ -44,9 +44,9 @@ class Solution
         {
             return 0;
         }
-        if(dp[i][j][k]!=-1)
+        if(dp[i][j]!=-1)
         {
-            return dp[i][j][k];
+            return dp[i][j];
         }
         int flag=0;
         if(i<s1.length() &&  k<s3.length()  && s1.charAt(i)==s3.charAt(k))
@@ -57,11 +57,8 @@ class Solution
         {
               flag|=isLetter(s1,s2,s3,i,j+1,k+1,dp);
         }
-        else
-        {
-            flag|=isLetter(s1,s2,s3,i+1,j,k,dp)|isLetter(s1,s2,s3,i,j+1,k,dp);
-        }
-        return dp[i][j][k]=flag;
+       
+        return dp[i][j]=flag;
     }
     public boolean isInterleave(String s1, String s2, String s3) 
     {
@@ -69,12 +66,12 @@ class Solution
         {
             return false;
         }
-        int dp[][][]=new int[s1.length()+1][s2.length()+1][s3.length()+1];
+        int[][] dp=new int[s1.length()+1][s2.length()+1];
         for(int i=0;i<=s1.length();i++)
         {
             for(int j=0;j<=s2.length();j++)
             {
-                Arrays.fill(dp[i][j],-1);
+                  dp[i][j]=-1;
             }
         }
         return isLetter(s1,s2,s3,0,0,0,dp)==1;
