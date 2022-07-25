6eeb798763a583addc52e801ef6d05dfc7143a9d
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-07-25 12:09:12 +0530

    Time: 157 ms (62.87%), Space: 137.6 MB (33.13%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,73 +1,60 @@
-class Solution
-{
-    public TreeSet<Integer> dfs(ArrayList<ArrayList<Integer>> adj,int src,int v[],ArrayList<TreeSet<Integer>> adj1)
-    {
-        if(adj.get(src).size()==0)
-        {
-           TreeSet<Integer> arp=new TreeSet<Integer>();
-            adj1.get(src).add(src);
-            arp.add(src);
-            return arp;
-        }
-        v[src]=1;
-        TreeSet<Integer> tq=new TreeSet<Integer>();
-        for(Integer a:adj.get(src))
-        {
-            if(v[a]==0)
-            {
-                TreeSet<Integer> arr=dfs(adj,a,v,adj1);
-                tq.addAll(arr);
-            }
-            else
-            { 
-                
-                tq.addAll(adj1.get(a));
-            }
-        }
-        tq.add(src);
-        adj1.get(src).addAll(tq);
-       
-      
-        return tq;
-    }
+class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges)
     {
-        
-        int m=edges.length;
-        ArrayList<ArrayList<Integer>> adj=new  ArrayList<ArrayList<Integer>>();
-        ArrayList<TreeSet<Integer>> adj1=new  ArrayList<TreeSet<Integer>>();
-
-        for(int i=0;i<n;i++)
+      ArrayList<ArrayList<Integer>> arr=new ArrayList<ArrayList<Integer>>();
+         for(int i=0;i<n;i++)
         {
-            adj.add(new ArrayList<Integer>());
-            adj1.add(new TreeSet<Integer>());
+            arr.add(new ArrayList<Integer>());
         }
         int indegree[]=new int[n];
-        for(int i=0;i<m;i++)
-        {
-            int u=edges[i][0];
-            int v=edges[i][1];
-            adj.get(v).add(u);
-            adj1.get(v).add(u);
-        }
-        
-       int v[]=new int[n];
+    for(int i=0;i<edges.length;i++)
+    {
+        int u=edges[i][0];
+        int v=edges[i][1];
+            arr.get(u).add(v);
+             
+           indegree[edges[i][1]]++;
+    }
+  ArrayList<TreeSet<Integer>> ans=new ArrayList<TreeSet<Integer>>(); 
         for(int i=0;i<n;i++)
         {
-            if(v[i]==0)
+            ans.add(new TreeSet<Integer>());
+        }
+        Queue<Integer> qu=new LinkedList<Integer>();
+                int v[]=new int[n];
+
+        for(int j=0;j<n;j++){
+            if(indegree[j]==0)
             {
-                dfs(adj,i,v,adj1);
+                qu.add(j);
+                v[j]=1;
             }
         }
-         List<List<Integer>> res=new  ArrayList<List<Integer>>();
-        for(int i=0;i<n;i++)
+        while(qu.isEmpty()==false)
         {
-            List<Integer> arr=new ArrayList<>(adj1.get(i));
-            arr.remove(new Integer(i));
-            res.add(arr);
+            int t=qu.poll();
+            for(Integer adj:arr.get(t))
+            {
+                if(v[adj]==0)
+                {
+                     indegree[adj]--;
+                     ans.get(adj).addAll(ans.get(t));
+                     ans.get(adj).add(t);
+                    if(indegree[adj]==0)
+                    {
+                        qu.add(adj);
+                        v[adj]=1;
+                    }
+                }
+            }
         }
-        return res;
-        }
-        
+          List<List<Integer>> ans1=new ArrayList<List<Integer>>(); 
+      for(int i=0;i<n;i++)
+      {
+          ans1.add(new ArrayList<Integer>(ans.get(i)));
+      }
+        return ans1;
         
     }
+    
+}
