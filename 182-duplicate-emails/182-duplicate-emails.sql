e6297d653c9a5ced8df4ea2dae1138991b9f870d
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-07-27 15:59:12 +0530

    Time: 302 ms (92.43%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,12 +1,4 @@
-# select Email from
-# (
-# select email,count(email) as num
-# from Person
-# group by email
-# )
-# as statastic
-# where num>1;
-select Email 
-from Person
-group by email
-Having count(email)>1;
+SELECT DISTINCT p1.Email
+FROM Person as p1 JOIN Person as p2
+ON p1.Email = p2.Email and p1.id != p2.id
+;
