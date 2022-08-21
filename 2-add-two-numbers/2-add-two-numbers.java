d17162eae7796a75bc5e882f06924ce8a5c50212
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-08-22 03:53:28 +0530

    Time: 3 ms (80.74%), Space: 47.9 MB (49.78%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -8,70 +8,50 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2)
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
     {
-        ListNode head=new ListNode(0);
+        
       
-                  ListNode aku=head;
-    
+        ListNode kapa=new ListNode(0);
+          ListNode a=kapa;
+        int sum=0;
         int carry=0;
         while(l1!=null && l2!=null)
         {
-           int val1=l1.val;
-           int val2=l2.val;
-           int sum=val1+val2+carry;
-            if(carry==1)
-            {
-                carry=0;
-            }
-           carry+=sum/10;
-           ListNode ap=new ListNode(sum%10);
-           aku.next=ap;
-           
-           aku=aku.next;
+            sum=l1.val+l2.val+carry;
             l1=l1.next;
-             l2=l2.next;
+            l2=l2.next;
+            ListNode b=new ListNode(sum%10);
+            a.next=b;
+            a=a.next;
+            carry=sum/10;
         }
-       
         while(l1!=null)
         {
-            int val1=l1.val;
-           int sum=val1+0+carry;
-            if(carry==1)
-            {
-                carry=0;
-            }
-           carry+=sum/10;
-           
-           ListNode ap=new ListNode(sum%10);
-           aku.next=ap;
-           aku=aku.next;
-                        l1=l1.next;
-
+          sum=l1.val+carry;
+          l1=l1.next;
+          ListNode b=new ListNode(sum%10); 
+          a.next=b;
+            a=a.next;
+          carry=sum/10;
         }
-          while(l2!=null)
+        while(l2!=null)
         {
-            int val2=l2.val;
-           int sum=val2+0+carry;
-if(carry==1)
-            {
-                carry=0;
-            }
-           carry+=sum/10;           ListNode ap=new ListNode(sum%10);
-           aku.next=ap;
-           aku=aku.next;
-                            l2=l2.next;
-
+          sum=l2.val+carry;
+          l2=l2.next;
+          ListNode b=new ListNode(sum%10); 
+          a.next=b;
+            a=a.next;
+          carry=sum/10;
         }
         if(carry==1)
         {
-            ListNode ap=new ListNode(1);
-           aku.next=ap;
-           
-           aku=aku.next;
+                      ListNode b=new ListNode(1); 
+             a.next=b;
+            a=a.next;
         }
-        return head.next;
-        
+        return kapa.next;
     }
 }
