0baa7821286c113a3587309ec82b5936aabaf0e0
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-08-22 04:15:51 +0530

    Time: 14 ms (49.05%), Space: 44.8 MB (57.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,43 +1,30 @@
 class Solution 
 {
-    public static boolean check(HashMap<Character,Integer> map)
-    {
-        for(Map.Entry<Character,Integer> m:map.entrySet())
-        {
-            if(m.getValue()>1)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+    
     public int lengthOfLongestSubstring(String s)
     {
-        int n=s.length();
-        int start=0;int end=0;
-HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+          int n=s.length();
+          HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+        int start=0;int end=-1;
         int max=0;
-    while(end<n)
+        while(end<n)
         {
-        
-               
-         
-                         
-
-        
-            
+            while(end-start+1>map.size())
+            {
+                if(map.get(s.charAt(start))>1)
+                map.put(s.charAt(start),map.get(s.charAt(start))-1);
+                else
+                map.remove(s.charAt(start));
+                start++;
+            }
+            max=Math.max(end-start+1,max);
+            end++;
+            if(end<n)
+            {
                  map.put(s.charAt(end),map.getOrDefault(s.charAt(end),0)+1);
-             
-         while(check(map)==false)
-                {
-                    map.replace(s.charAt(start),map.get(s.charAt(start))-1);
-                    start++;
-                }
-         max=Math.max(end-start+1,max);
-          end++;
-
+            }
         }
-      
         return max;
+        
     }
 }
