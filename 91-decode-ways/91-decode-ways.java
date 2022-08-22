4af665effc26c132d9d58e4c3d0fedcc4643c853
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-08-22 23:41:17 +0530

    Time: 18 ms (5.75%), Space: 53.9 MB (5.08%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,36 +1,35 @@
 class Solution 
 {
-    public static int val(String s,int pos,int j,HashMap<String,String> map,int dp[])
+    public static int val(String s,int pos,int j,int dp[])
     {
-        if(pos>=j)
+        if(pos==j+1)
         {
             return 1;
         }
+        int total=0;
         if(dp[pos]!=-1)
         {
             return dp[pos];
         }
-        int total=0;
-        for(int i=pos;i<s.length();i++)
+        for(int i=pos;i<=j;i++)
         {
-            String s1=s.substring(pos,i+1);
-            if(map.containsKey(s1)==true)
+            String str=s.substring(pos,i+1);
+ 
+            if(str.length()<=2 && str.charAt(0)!='0')
             {
-                total+=val(s,i+1,j,map,dp);
+                int t=Integer.parseInt(str);
+                if(t<=26 && t>=1)
+                {
+                    total+=val(s,i+1,j,dp);
+                }
             }
         }
         return dp[pos]=total;
     }
     public int numDecodings(String s) 
     {
-        
-        HashMap<String,String> map=new HashMap<String,String>();
-        for(int i=1;i<=26;i++)
-        {  
-           map.put(Integer.toString(i),"A");
-        }
         int dp[]=new int[s.length()+1];
         Arrays.fill(dp,-1);
-        return val(s,0,s.length(),map,dp);
+        return val(s,0,s.length()-1,dp);
     }
 }
