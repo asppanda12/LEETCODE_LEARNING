bb6eab5005beb2db1ce7e06d2834e7373dad13c5
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-08-31 17:25:50 +0530

    Time: 716 ms (55.36%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,16 +1,9 @@
-# Write your MySQL query statement 
-select s.department as 'Department',Employee.name as 'Employee',s.salary as 'Salary'
-
-    from Employee JOIN (
-
-
-
-select table1.DepartmentId,table1.salary, Department.name as department
-from Department  inner join
-(SELECT DepartmentId, name,MAX(salary) as 'Salary'
-from Employee 
-group by departmentId)table1 on Department.id = table1.DepartmentId)
-
-
-
-s on Employee.departmentId=s.DepartmentId AND Employee.salary =s.salary ; 
+select table2.Department,e2.name as'Employee',table2.salary as 'Salary'
+from Employee as e2 INNER JOIN (
+select Department.id as 'id',Department.name as 'Department',table1.max_salary as 'salary'
+from Department inner join
+(select e1.departmentId as 'id',MAX(e1.salary) as 'max_salary'
+from Employee as e1
+group by departmentId)table1
+on table1.id=Department.id) table2 
+ON e2.departmentId =table2.id and e2.salary=table2.salary;
