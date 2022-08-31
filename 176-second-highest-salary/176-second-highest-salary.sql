dc7e32cbde7ef460b888e46c3e35eb435402be8b
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-08-31 10:54:57 +0530

    Time: 258 ms (59.59%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,11 +1,6 @@
 # Write your MySQL query statement below
-select ifnull(NULL,
-(
-select t1.salary 
-from(
-    (select  salary,Dense_Rank() OVER (ORDER BY salary desc) as 'rank'
-    from Employee)
-  )t1
-  where t1.rank = 2
-  limit 1
-) )as 'SecondHighestSalary';
+
+
+select Max(salary) as 'SecondHighestSalary'
+from Employee
+where salary <> (select MAX(salary) from Employee);
