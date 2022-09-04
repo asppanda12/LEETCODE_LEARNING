a2ad6486a6b2313ef33b9c70e7a88f8319c537dc
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-09-04 23:08:57 +0530

    Time: 6 ms (47.86%), Space: 43.3 MB (39.33%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -17,77 +17,62 @@ class Solution
 {
     class Pair
     {
-        int ind;
-        TreeNode root1;
-        Pair(TreeNode root1,int ind)
+        TreeNode x;
+        int y;
+        Pair(TreeNode x,int y)
         {
-            this.ind=ind;
-            this.root1=root1;
+            this.x=x;
+            this.y=y;
         }
     }
-    public void traverse(TreeMap<Integer,Integer> map,TreeNode root,TreeMap<Integer,ArrayList<Integer>> map2)
+    
+    public List<List<Integer>> verticalTraversal(TreeNode root) 
     {
-       Queue<Pair> qu=new LinkedList<Pair>();
-       qu.add(new Pair(root,0));
-        map2.put(0,new ArrayList<Integer>());
-                            map2.get(0).add(root.val);
- map.put(0,root.val);
+        TreeMap<Integer,ArrayList<Integer>> map1=new TreeMap<>();
+        Queue<Pair> qu=new LinkedList<Pair>();
+        qu.add(new Pair(root,0));
         while(qu.isEmpty()==false)
         {
-             int size=qu.size();
-            TreeMap<Integer,ArrayList<Integer>> map1=new TreeMap<>();
-            for(int i=0;i<size;i++)
+           TreeMap<Integer,ArrayList<Integer>> map=new TreeMap<Integer,ArrayList<Integer>>(); 
+           int size=qu.size();
+           for(int i=0;i<size;i++)
+           {
+                 TreeNode root1=qu.peek().x;
+                 int val1=qu.peek().y;
+                 qu.poll();
+                  if(map.containsKey(val1)==false)
+                  {
+                      map.put(val1,new ArrayList<Integer>());
+                  }
+               map.get(val1).add(root1.val);
+                if(root1.left!=null)
+                 {
+                     qu.add(new Pair(root1.left,val1-1));
+                 }
+                 if(root1.right!=null)
+                 {
+                     qu.add(new Pair(root1.right,val1+1));
+                 }
+           }
+            for(Map.Entry<Integer,ArrayList<Integer>> m:map.entrySet())
             {
-                Pair t=qu.peek();
-                qu.poll();
-                if(t.root1.left!=null)
-                {
-                    if(map.containsKey(t.ind-1)==false)
-                    map.put(t.ind-1,t.root1.left.val);
-                    if(map1.containsKey(t.ind-1)==false)
-                    {
-                        map1.put(t.ind-1,new ArrayList<Integer>());
-                    }
-                    map1.get(t.ind-1).add(t.root1.left.val);
-                    qu.add(new Pair(t.root1.left,t.ind-1));
-                }
-                if(t.root1.right!=null)
-                {
-                    if(map.containsKey(t.ind+1)==false)
-                    map.put(t.ind+1,t.root1.right.val);
-                     if(map1.containsKey(t.ind+1)==false)
-                    {
-                        map1.put(t.ind+1,new ArrayList<Integer>());
-                    }
-                    map1.get(t.ind+1).add(t.root1.right.val);
-                    qu.add(new Pair(t.root1.right,t.ind+1));
-                }
+                int key=m.getKey();
+                ArrayList<Integer> ap=m.getValue();
+                if(map1.containsKey(key)==false)
+                  {
+                      map1.put(key,new ArrayList<Integer>());
+                  }
+               ArrayList<Integer> arr=new ArrayList<>(ap);
+                Collections.sort(arr);
+                map1.get(key).addAll(arr);
+
             }
-            for(Map.Entry<Integer,ArrayList<Integer>> ma:map1.entrySet())
-        {
-               if(map2.containsKey(ma.getKey())==false)
-               {
-                   map2.put(ma.getKey(),new ArrayList<Integer>());
-               }
-                ArrayList<Integer> ap1=new ArrayList<Integer>(ma.getValue());
-                Collections.sort(ap1);
-                map2.get(ma.getKey()).addAll(ap1);
         }
-            
-        }
-        
-    }
-    public List<List<Integer>> verticalTraversal(TreeNode root)
-    {
-        TreeMap<Integer,Integer> map=new TreeMap<Integer,Integer>();     
-        TreeMap<Integer,ArrayList<Integer>> map1=new TreeMap<Integer,ArrayList<Integer>>();   
-        traverse(map,root,map1);
-        List<List<Integer>> arr=new ArrayList<List<Integer>>();
-        System.out.println(map);
+        List<List<Integer>> ls=new ArrayList<>();
         for(Map.Entry<Integer,ArrayList<Integer>> ma:map1.entrySet())
         {
-               arr.add((List)(ma.getValue()));
+             ls.add(ma.getValue());
         }
-        return arr;
+        return ls;
     }
 }
