abcca8628c5e5b1228b7beb52834461e5c992fa5
asppanda12 <57539578+asppanda12@users.noreply.github.com>
2022-09-13 14:02:14 +0530

    Time: 0 ms (100.00%), Space: 60.4 MB (57.94%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -3,45 +3,42 @@ class Solution {
     {
         int n=nums.length;
         int lo=0;int hi=n-1;
-        while(lo<hi)
-        {
-            int mid=(lo+hi)/2;
-            if(mid%2==0)
-            {
-                if(mid>0 && nums[mid]==nums[mid-1])
-                {
-                    hi=mid-1;
-                }
-                else
-                if(mid<n && nums[mid]==nums[mid+1])
-                {
-                    lo=mid+1;
-                }
-                else
-                {
-                    return nums[mid];
-                }
-
-            }
-            else
-            {
-                
-                if(mid>0 && nums[mid]==nums[mid-1])
-                {
-                    lo=mid+1;
-                }
-                else
-                if(mid<n && nums[mid]==nums[mid+1])
-                {
-                    hi=mid-1;
-                }
-                else
-                {
-                    return nums[mid];
-                }
-
-            }
-        }
+       while(lo<hi)
+       {
+           int mid=(lo+hi)/2;
+           if(mid%2==0)
+           {
+               if(mid>0 && nums[mid]==nums[mid-1])
+               {
+                   hi=mid-1;
+               }
+               else
+               if(mid<n && nums[mid]==nums[mid+1])
+               {
+                   lo=mid+1;
+               }
+               else
+               {
+                   return nums[mid];
+               }
+           }
+           else
+           {
+               if(mid>0 && nums[mid]==nums[mid-1])
+               {
+                   lo=mid+1;
+               }
+               else
+               if(mid<n && nums[mid]==nums[mid+1])
+               {
+                   hi=mid-1;
+               }
+               else
+               {
+                   return nums[mid];
+               }
+           }
+       }
         return nums[lo];
         
     }
